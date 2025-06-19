2b8520d1468e2ca6c4ae6d122bd7790b7039da48
Rususlan <ruslankapustian001@gmail.com>
2025-06-20 02:31:13 +0300

    5

--- start-react/src/component/Section.jsx
+++ start-react/src/component/Section.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
 
 const Section = (props) => {
+    const janr = ["Drama", "Thriller", "Supernatural,", "Mystery", "Sci-Fi"];
+    const ratings = [1, 2, 3, 4, 5];
+    const handleStarClick = (rating) => {
+        alert(`You rated this ${rating}`);
+    };
+    
+    const RbtnClick = () =>{
+        alert("This button doesn't do anything yet.");
+    }
+    const AbtnClick = () =>{
+        alert("This button does the same thing as the red button.");
+    }
     return (
         <section>
                 <div class="genres">
-                    <span>Drama</span>
-                    <span>Thriller</span>
-                    <span>Supernatural</span>
+                    {janr.map(gan => <span key={gan} >{gan}</span>)}
                 </div>
                 <div class="MainName">
                     <h1>
@@ -24,33 +34,20 @@ const Section = (props) => {
                     </p>
                 </div>
                 <div class="stars">
-                    <span class="star">
-                    <i class="fa-regular fa-star"></i>
-                    <i class="fa-solid fa-star"></i>
+                    {ratings.reverse().map(rating => 
+                    <span class="star" onClick={() => handleStarClick(rating)}>
+                        <i class="fa-regular fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                        
                     </span>
-                    <span class="star">
-                    <i class="fa-regular fa-star"></i>
-                    <i class="fa-solid fa-star"></i>
-                    </span>
-                    <span class="star">
-                    <i class="fa-regular fa-star"></i>
-                    <i class="fa-solid fa-star"></i>
-                    </span>
-                    <span class="star">
-                    <i class="fa-regular fa-star"></i>
-                    <i class="fa-solid fa-star"></i>
-                    </span>
-                    <span class="star">
-                    <i class="fa-regular fa-star"></i>
-                    <i class="fa-solid fa-star"></i>
-                    </span>
+                    )}
                 </div>
                 <div class="buttons">
-                    <button class="btn btn-primary">
-                    STREAM NOW <i class="fa fa-play"></i>
+                    <button class="btn btn-primary" onClick={RbtnClick}>
+                        STREAM NOW <i class="fa fa-play"></i>
                     </button>
-                    <button class="btn btn-secondary">
-                    ALL EPISODES
+                    <button class="btn btn-secondary" onClick={AbtnClick}>
+                        ALL EPISODES
                     </button>
                 </div>
             </section>
